2eb71a62f9395fc1b5f32c38206674c4a0538774
Omar Vázquez <ovazquez@eng.ucsd.edu>
2021-05-27 07:57:21 -0700

    Create api.js

---
+++ loginapp/api.js
@@ -0,0 +1,6 @@
+// POST Request.
+fetch('http://192.168.0.202:51007')
+    // Handle success
+    .then(response => response.json())  // convert to json
+    .then(json => console.log(json))    //print data to console
+    .catch(err => console.log('Request Failed', err)); // Catch errors
